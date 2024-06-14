5a583d79ce79c87c6bc0ea0bfca46174268ea3c7
gauravkatara53 <gauravkatara53@gmail.com>
2024-06-14 09:22:40 +0530

    assign

--- Week 10 Linked List/class 4/linkedListCycle.cpp
+++ Week 10 Linked List/class 4/linkedListCycle.cpp
@@ -4,7 +4,7 @@
 
 // There is a cycle in a linked list if there is some node in the list that can be reached again by continuously following the next pointer. Internally, pos is used to denote the index of the node that tail's next pointer is connected to. Note that pos is not passed as a parameter.
 
-// Return true if there is a cycle in the linked list. Otherwise, return false.
+//  Return true if there is a cycle in the linked list. Otherwise, return false.
 
 #include<iostream>
 using namespace std;
